e500c6dbdee19d51b8efcc54ae7e7a81186f286a
cloudhuh <claudiu.huhulia@gmail.com>
2023-07-10 19:28:46 +0200

    fixed svg divisor

--- src/components/AppAward.vue
+++ src/components/AppAward.vue
@@ -7,12 +7,21 @@
         <div class="my-btn mt-2">
             MEET YOUR NEW BARBER
         </div>
+
+
     </main>
+    <svg class="fusion-big-triangle-candy " xmlns="http://www.w3.org/2000/svg" version="1.1" width="100%" height="100"
+        viewBox="0 0 100 100" preserveAspectRatio="none" style="fill:#ffffff;padding:0;">
+        <path d="M0 100 L50 2 L100 100 Z"></path>
+    </svg>
 </template>
 
 <style scoped lang="scss">
 @use '../assets/scss/style.scss' as *;
 
+svg {
+    background-color: #171717
+}
 
 .my-btn {
     width: 300px;
