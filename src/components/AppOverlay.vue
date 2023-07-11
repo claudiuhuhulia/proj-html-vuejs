99c4d3083638cbfc8e9d5a8863762439cd31b24a
cloudhuh <claudiu.huhulia@gmail.com>
2023-07-11 18:18:39 +0200

    fixed overlay content

--- src/components/AppOverlay.vue
+++ src/components/AppOverlay.vue
@@ -5,17 +5,20 @@ export default {
 
 </script>
  
- 
+
 <template>
     <div class="overlay-nav d-flex flex-column align-items-center justify-content-center">
 
-        <div class="close-btn" @click="$emit('close-clicked')">
-
-            <font-awesome-icon icon="fa-solid fa-rectangle-xmark" class="close-btn" />
-        </div>
+        <font-awesome-icon icon="fa-solid fa-rectangle-xmark" class="close-btn" @click="$emit('close-clicked')" />
 
 
-
+        <a href="#" id="home">Home</a>
+        <a href="#" id="aboutus">About Us</a>
+        <a href="#">Serices</a>
+        <a href="#">Shop</a>
+        <a href="#">Our Team</a>
+        <a href="#">Blog</a>
+        <a href="#">Contact Us</a>
 
     </div>
 </template>
@@ -32,6 +35,21 @@ export default {
     right: 0;
     background-color: black;
 }
+
+a {
+    text-decoration: none;
+    color: white;
+    font-size: 1.7rem;
+    padding-bottom: 10px;
+
+    &:hover {
+        color: rgb(108, 108, 218);
+    }
+
+
+}
+
+
 
 .close-btn {
     font-size: 3rem;
