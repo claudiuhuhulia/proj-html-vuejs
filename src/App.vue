1df6ddc86275e84925be94c162ecf50db29cdf52
cloudhuh <claudiu.huhulia@gmail.com>
2023-07-11 12:02:09 +0200

    added posts section

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,9 @@ import AppAward from './components/AppAward.vue';
 import AppGrooming from './components/AppGrooming.vue';
 import AppShop from './components/AppShop.vue';
 import AppReviw from './components/AppReviews.vue'
+import AppPosts from './components/AppPosts.vue'
 export default {
-  components: { AppHeader, AppFooter, AppAward, AppGrooming, AppShop, AppReviw }
+  components: { AppHeader, AppFooter, AppAward, AppGrooming, AppShop, AppReviw, AppPosts }
 }
 
 </script>
@@ -21,6 +22,8 @@ export default {
   <AppShop />
 
   <AppReviw />
+
+  <AppPosts />
 </template>
 
 <style lang="scss">
---
+++ src/components/AppPosts.vue
@@ -0,0 +1,43 @@
+<script></script>
+
+<template>
+    <main>
+        <div class="title-section d-flex justify-content-center align-items-center flex-column ">
+            <h5>TALES FROM THE BARBER SHOP</h5>
+            <h1>Recent Blog Posts</h1>
+        </div>
+
+        <section class="container grooming-services d-flex justify-content-between mt-5 ">
+            <div class="my-card"></div>
+            <div class="my-card"></div>
+            <div class="my-card"></div>
+        </section>
+
+
+        <div class="footer-section d-flex justify-content-center my-5">
+            <div class="my-btn my-5">
+                READ MORE FROM OUR BLOG
+            </div>
+        </div>
+    </main>
+</template>
+
+<style scoped lang="scss">
+@use '../assets/scss/style.scss' as *;
+
+main {
+    background-color: #f5f5f5;
+}
+
+.title-section {
+    padding-top: 100px;
+}
+
+.my-btn {
+    width: 350px;
+}
+
+.footer-section {
+    padding-bottom: 75px;
+}
+</style>
